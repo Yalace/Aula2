4521fee1166ba214c84ed44d1647168574d7d6bd
2022101332 <2022101332@LAB2-013>
2023-08-22 20:14:40 -0300

    Adicionar arquivos de projeto.

---
+++ Aula2/Conversor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula2
+{
+    public class Conversor
+    {
+        public static double converte(double n1)
+        {
+            return n1 * 1000;
+        } 
+       
+    }
+}
---
+++ Aula2/Program.cs
@@ -0,0 +1,18 @@
+﻿using Aula2;
+
+int num1 = 20;
+int num2 = 11;
+double result;
+
+Somador soma;
+soma = new Somador();
+
+result = soma.Soma(num1 , num2);
+Console.WriteLine($"a soma de {num1} + {num2} é {result}");
+
+
+
+
+
+double n1 = 60;
+Console.WriteLine("a medida {0} metros corresponde a {1}",n1,Conversor.converte(n1));
